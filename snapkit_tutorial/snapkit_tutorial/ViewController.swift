155c5c6a12f72989ed02e83f843c852d484a4197
luminoux <yungzin98@naver.com>
2022-10-01 18:06:37 +0900

    chore: initial commit
    + 프로젝트 생성

---
+++ snapkit_tutorial/snapkit_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapkit_tutorial
+//
+//  Created by YeongJin Jeong on 2022/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
