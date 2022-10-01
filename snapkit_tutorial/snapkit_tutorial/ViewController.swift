97c123629385dda3fb829123089010d167d54119
luminoux <yungzin98@naver.com>
2022-10-01 22:17:54 +0900

    Feat: Preview 추가
    + UIKit에서 SwiftUI의 Preview를 사용할 수 있도록 랜더링

--- snapkit_tutorial/snapkit_tutorial/ViewController.swift
+++ snapkit_tutorial/snapkit_tutorial/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var greenBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .green 
+        return view
+
+
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +26,32 @@ class ViewController: UIViewController {
 
 }
 
+
+
+#if DEBUG
+
+import SwiftUI
+struct ViewControllerReperesentable: UIViewControllerRepresentable {
+
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // empty
+    }
+
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController {
+        ViewController()
+    }
+}
+@available(iOS 13.0.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerReperesentable()
+                .ignoresSafeArea()
+                .previewDisplayName("Preview")
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13"))
+        }
+    }
+}
+#endif
